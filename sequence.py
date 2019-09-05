98676b736a891ff1a32d5ebf9a69a2c01b351160
Thorkell <thorkell19@ru.is>
2019-09-05 10:20:19 +0000

    Algorithm written for max_int

--- sequence.py
+++ sequence.py
@@ -3,3 +3,10 @@
 # saman næstu þrjár tölur á undan.
 # Láta tölvuna gera þetta þangað til lúppan er 
 # búin að gerast jafn oft og lengd rununnar á að vera.
+# Ath. að fyrstu þrjú case-in eru special cases.
+
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+sum3 = 1
+first = 0
+second = 0
+third = 0
