8f5641e1a7800aa96b6adb97db5d3a78b0fb6a84
Thorkell <thorkell19@ru.is>
2019-09-05 09:37:05 +0000

    Initial commit

---
+++ max_int.py
@@ -0,0 +1,4 @@
+# Fá heiltölu í input.
+# Bera inputið saman við það sem er í breytu sem heitir max.
+# Ef inputið er hærra en maxið þá tekur maxið gildið sem er í inputinu.
+# Þetta ferli er endurtekið þangað til inputið er mínustala.
