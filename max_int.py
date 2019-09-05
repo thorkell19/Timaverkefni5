41992542b3729b482524cea82f5ef27e7564f331
Thorkell <thorkell19@ru.is>
2019-09-05 09:47:42 +0000

    Algorithm written for max_int

--- max_int.py
+++ max_int.py
@@ -1,4 +1,18 @@
 # Fá heiltölu í input.
+# Búa til breytu sem heitir max.
 # Bera inputið saman við það sem er í breytu sem heitir max.
 # Ef inputið er hærra en maxið þá tekur maxið gildið sem er í inputinu.
 # Þetta ferli er endurtekið þangað til inputið er mínustala.
+
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+
+max_int = 0
+
+while num_int >= 0:
+    if max_int < num_int:
+        max_int = num_int
+    num_int = int(input("Input a number: "))
+
+print("The maximum is", max_int)    # Do not change this line
+
