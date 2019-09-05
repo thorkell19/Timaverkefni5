5513729eaa39346ee7d093fe301e2dee3fa7af09
Thorkell <thorkell19@ru.is>
2019-09-05 10:38:41 +0000

    Veit ekki neitt ennþá

--- sequence.py
+++ sequence.py
@@ -6,7 +6,29 @@
 # Ath. að fyrstu þrjú case-in eru special cases.
 
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
-sum3 = 1
-first = 0
-second = 0
-third = 0
+
+for i in range(1,n+1):
+    if i == 1:
+        first = 1
+        second = 0
+        third = 0
+        sum3= first + second + third
+        print(sum3, end=", ")
+    elif i == 2:
+        first = 1
+        second = 1
+        third = 0
+        sum3= first + second + third
+        print(sum3, end=", ")
+    elif i == 3:
+        first = 1
+        second = 1
+        third = 1
+        sum3= first + second + third
+        print(sum3, end=", ")
+    else:
+        first = i-2
+        second = i-1
+        third = i
+        sum3= first + second + third
+        print(sum3, end=", ")
