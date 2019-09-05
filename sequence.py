1935326b9905528ed705057591fd7a7670e04fcd
Thorkell <thorkell19@ru.is>
2019-09-05 09:56:19 +0000

    Initial commit

---
+++ sequence.py
@@ -0,0 +1,5 @@
+# Taka inn input sem er lengd rununnar.
+# Leggja saman tölur þannig að það leggur 
+# saman næstu þrjár tölur á undan.
+# Láta tölvuna gera þetta þangað til lúppan er 
+# búin að gerast jafn oft og lengd rununnar á að vera.
